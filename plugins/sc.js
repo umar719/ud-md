e1c308f17483e1fbdf83a5607ee4ffb5e13663c0
U M A R D E V I L <103053269+umar404@users.noreply.github.com>
2022-09-05 14:30:50 +0500

    Add files via upload

---
+++ plugins/sc.js
@@ -0,0 +1,34 @@
+let handler = async (m, { conn, usedPrefix: _p }) => {
+
+
+let buttonMessage= {
+'document':{'url': 'http://s.id/ud-md' },
+'mimetype': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+'fileName': `「  𝑯𝒆𝒍𝒍𝒐 𝑾𝒐𝒓𝒍𝒅 𝐒𝐮𝐛 𝐌𝐲 𝐘𝐭」`,
+'fileLength': 22222222222222,
+'pageCount': 222,
+'contextInfo':{
+'forwardingScore':222,
+'isForwarded':true,
+'externalAdReply':{
+'mediaUrl': 'http://github.com/Kyu/Batao/umar/sai/contact/karoo😹',
+'mediaType': 2,
+'previewType': 2,
+'title': '𝐷𝛢𝛤𝛫  𝐷𝜩𝛻𝜤𝐿 👑 🇵🇰 ❤️',
+'body': me,
+'thumbnail': thumb2,
+'sourceUrl': 'https://www.youtube.com/c/UMARMODS'}},
+'caption': 'http://github.com/Kyu/Batao/umar/sai/contact/karoo😹',
+'footer': me,
+'buttons':[
+{'buttonId': _p + 'menu','buttonText':{'displayText':'ᴍᴇɴᴜ😈'},'type':1},
+{'buttonId': _p + 'runtime','buttonText':{'displayText':'ʀᴜɴᴛɪᴍᴇ🏃‍♂️'},'type':1}
+],
+'headerType':6}
+    await conn.sendMessage(m.chat,buttonMessage, { quoted: m })
+
+}
+
+handler.command = /^(sc)$/i
+
+export default handler
