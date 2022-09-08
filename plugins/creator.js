02427e4c06f8aadda07a98d09e06c7e33ccbb14d
U M A R D E V I L <103053269+umar404@users.noreply.github.com>
2022-09-09 00:32:50 +0500

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,13 +1,12 @@
 function handler(m) {
-/*  
+  
   const kontak = {
-	"displayName": 'Dark Devil Umar',
-	vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;;;;\nFN:${conn.getName('923165123719@s.whatsapp.net')}\nitem1.TEL;waid=923165123719:923165123719\nitem1.X-ABLabel:Busy.\nURL;My Yt: https://www.youtube.com/c/UMARMODS\nEMAIL;Email Owner: umardevil48@gamil.com\nORG: Save My  + Owner number\nTEL;My number ;waid=923165123719:923165123719\nEND:VCARD`
+	"displayName": 'My Owner Umar',
+	vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;;;;\nFN:${conn.getName('923165123719@s.whatsapp.net')}\nitem1.TEL;waid=923165123719:923165123719\nitem1.X-ABLabel:save-me.\nURL;My Web: https://hi.umar404.my.id\nEMAIL;Email Owner: me@umar404.my.id\nORG: NOT A BOT +SAVE ME\nTEL;My number ;waid=4917396999993:4917396999993\nEND:VCARD`
 }
 
 conn.sendMessage(m.chat, { contacts: { contacts: [kontak] }}, { quoted: m })
-  */
-this.sendContact(m.chat, [['923165123719', 'Owner']], m)
+  
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
