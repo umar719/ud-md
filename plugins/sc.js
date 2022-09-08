b0b1d705f3a047423e6c8117af2831828fc13bdd
U M A R D E V I L <103053269+umar404@users.noreply.github.com>
2022-09-09 00:28:26 +0500

    Update sc.js

--- plugins/sc.js
+++ plugins/sc.js
@@ -4,25 +4,25 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
 let buttonMessage= {
 'document':{'url': 'http://s.id/ud-md' },
 'mimetype': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-'fileName': `「  𝑯𝒆𝒍𝒍𝒐 𝑾𝒐𝒓𝒍𝒅 𝐒𝐮𝐛 𝐌𝐲 𝐘𝐭」`,
+'fileName': `「  𝑯𝒆𝒍𝒍𝒐 𝑾𝒐𝒓𝒍𝒅 」`,
 'fileLength': 22222222222222,
 'pageCount': 222,
 'contextInfo':{
 'forwardingScore':222,
 'isForwarded':true,
 'externalAdReply':{
-'mediaUrl': 'http://github.com/umar404',
+'mediaUrl': 'http://github.com/umar404/ud-md',
 'mediaType': 2,
 'previewType': 2,
-'title': '𝐷𝛢𝛤𝛫  𝐷𝜩𝛻𝜤𝐿 👑 🇵🇰 ❤️',
+'title': 'Searching Source Code?',
 'body': me,
 'thumbnail': thumb2,
 'sourceUrl': 'https://www.youtube.com/c/UMARMODS'}},
-'caption': 'http://github.com/umar404',
+'caption': 'http://github.com/umar404/ud-md',
 'footer': me,
 'buttons':[
-{'buttonId': _p + 'menu','buttonText':{'displayText':'ᴍᴇɴᴜ😈'},'type':1},
-{'buttonId': _p + 'runtime','buttonText':{'displayText':'ʀᴜɴᴛɪᴍᴇ🏃‍♂️'},'type':1}
+{'buttonId': _p + 'menu','buttonText':{'displayText':'ᴍᴇɴᴜ'},'type':1},
+{'buttonId': _p + 'runtime','buttonText':{'displayText':'ʀᴜɴᴛɪᴍᴇ'},'type':1}
 ],
 'headerType':6}
     await conn.sendMessage(m.chat,buttonMessage, { quoted: m })
