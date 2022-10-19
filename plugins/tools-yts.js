a51f54b2d95bfba03bb28a9de856450c7e681a9f
U M A R D E V I L <103053269+umar404@users.noreply.github.com>
2022-10-19 13:15:09 +0500

    Update tools-yts.js

--- plugins/tools-yts.js
+++ plugins/tools-yts.js
@@ -1,19 +1,19 @@
 import { youtubeSearch } from '@bochilteam/scraper'
 let handler = async (m, { text }) => {
-  if (!text) throw 'Cari apa?'
+  if (!text) throw 'Find what?'
   const { video, channel } = await youtubeSearch(text)
   let teks = [...video, ...channel].map(v => {
     switch (v.type) {
       case 'video': return `
-➠ *${v.title}* (${v.url})
-➠ Duration: ${v.durationH}
-➠ Uploaded ${v.publishedTime}
-➠ ${v.view} views
+🍫 *${v.title}* (${v.url})
+🍫 Duration: ${v.durationH}
+🍫 Uploaded ${v.publishedTime}
+🍫 ${v.view} views
       `.trim()
       case 'channel': return `
-➠ *${v.channelName}* (${v.url})
-➠ _${v.subscriberH} (${v.subscriber}) Subscriber_
-➠ ${v.videoCount} video
+🍫 *${v.channelName}* (${v.url})
+🍫 _${v.subscriberH} (${v.subscriber}) Subscriber_
+🍫 ${v.videoCount} video
 `.trim()
     }
   }).filter(v => v).join(`\n━━━━━━━━━━━━━━━━━━━━━━\n`)
