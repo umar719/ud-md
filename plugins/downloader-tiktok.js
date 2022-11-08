8b00e7276b4469f7584f703ed066b0e4ee93f243
U M A R D E V I L <103053269+umar404@users.noreply.github.com>
2022-11-08 21:04:47 +0500

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,32 +1,13 @@
-import fetch from 'node-fetch'
-
-let handler = async (m, { conn, text, usedPrefix }) => {
-	if (!text) throw 'Input URL' 
-	if (!/(?:https:?\/{2})?(?:w{3}|vm|vt|t)?\.?tiktok.com\/([^\s&]+)/gi.test(text)) throw 'Invalid URL'
-	let url = (await fetch(text)).url
-	let res = await (await fetch(`https://api2.musical.ly/aweme/v1/aweme/detail/?aweme_id=${url.split('?')[0].split('/')[5]}`)).json()
-	let data = res.aweme_detail.video.play_addr.url_list
-	if (!data.length) throw 'Can\'t download video!'
-	let meta = await getInfo(url).catch(_ => {})
-	await m.reply('_In progress, please wait..._')
-	let buttons = [{ buttonText: { displayText: 'Audio' }, buttonId: `${usedPrefix}tomp3` }]
-	conn.sendMessage(m.chat, { video: { url: data[data.length - 1] }, caption: meta?.description || null, footer: await shortUrl(data[data.length - 1]), buttons }, { quoted: m })
-	// conn.sendMessage(m.chat, { video : { url: res.link }, caption: description }, { quoted: m })
+import api from 'caliph-api'
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) throw `Link tiktoknya mana?\n\ncontoh:\n${usedPrefix}${command} https://vt.tiktok.com/ZSdK7vr3C/?k=1`
+    let res = await api.downloader.tiktok(args[0])
+    let url = res.nowm
+    await conn.sendFile(m.chat, url, '', `*DONE*`, m)
 }
-handler.help = ['tiktok']
+handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.alias = ['tiktok', 'tikdl', 'tiktokdl', 'tiktoknowm']
-handler.command = /^(tt|tiktok)(dl|nowm)?$/i
+
+handler.command = /^(tik|tt|tiktok)$/i
 
 export default handler
-
-async function getInfo(url) {
-	// url = (await fetch(url)).url
-	let id = url.split('?')[0].split('/')
-	let res = await (await fetch(`https://www.tiktok.com/node/share/video/${id[3]}/${id[5]}/`)).json()
-	return res?.seoProps?.metaParams
-}
-
-async function shortUrl(url) {
-	return await (await fetch(`https://tinyurl.com/api-create.php?url=${url}`)).text()
-}
