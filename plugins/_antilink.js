d07ba9136e2e6fe564ce8044b2df0caa638e5780
U M A R D E V I L <103053269+umar404@users.noreply.github.com>
2022-11-13 11:53:57 +0500

    Create _antilink.js

---
+++ plugins/_antilink.js
@@ -0,0 +1,23 @@
+const linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
+
+export async function before(m, { conn, isAdmin, isBotAdmin }) {
+    if (m.isBaileys && m.fromMe)
+        return !0
+    if (!m.isGroup) return !1
+    let chat = db.data.chats[m.chat]
+    let bot = db.data.settings[this.user.jid] || {}
+    const isGroupLink = linkRegex.exec(m.text)
+
+    if (chat.antiLink && isGroupLink && !isAdmin) {
+        if (isBotAdmin) {
+            const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
+            if (m.text.includes(linkThisGroup)) return !0
+        }
+        await conn.reply(m.chat, `*Group link detect!*${isBotAdmin ? '' : '\n\n_Bot not admin_  t_t'}`, m)
+        await await conn.sendMessage(m.chat, { delete: m.key })
+        if (isBotAdmin && bot.restrict) {
+            await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+        } else if (!bot.restrict) return false
+    }
+    return !0
+}
